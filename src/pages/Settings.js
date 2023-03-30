d64e565c2287be4816e35b27971ebbdf696817dc
abeshilu <102710800+abeshilu@users.noreply.github.com>
2023-03-30 16:42:52 -0500

    idk

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,12 +1,17 @@
 import './Settings.css';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Card, Container, Image } from 'react-bootstrap';
 
 function Settings() {
   return (
     <Container>
       <h1 id="titleText">Settings</h1>
-      <p>This is currently only a placeholder page.</p>
+      <div>
+        <Image className='account-image' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' roundedCircle fluid thumbnail></Image>
+        <Card>
+
+        </Card>
+      </div>
     </Container>
   );
 }
