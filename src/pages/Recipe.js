12562ad632cdbab43ae8c5651fff2c29dde73dea
Grant Shirah <gmshirah@gmail.com>
2023-03-30 20:52:33 -0500

    Edited placeholder recipe page

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -2,78 +2,46 @@ import './Recipe.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import {
+  Accordion,
   Button,
   Container,
   Form,
   InputGroup,
   Row,
   Col,
-  Image
+  Image,
+  Card
 } from 'react-bootstrap';
-
-import { Accordion, AccordionSummary, Typography, AccordionDetails, responsiveFontSizes } from '@mui/material';
-import { borderRadius } from '@mui/system';
 import backButton from "../assets/back-arrow.png";
 import chickenPic from "../assets/orangechicken.png";
 import savedButton from "../assets/save-star.png";
 import savedYellowButton from "../assets/save-star-yellow.png"
 
-
-
-
-
 const Ingredient = ({ ingredient }) => {
   return (
-
-    <Accordion
-      sx={{
-        backgroundColor: "#33DB80",
-        borderRadius: "5px",
-        borderCollapse: "separate"
-      }}
-    >
-      <AccordionSummary
-        sx={{
-          borderColor: "black",
-          borderWidth: "2px",
-          borderStyle: "solid",
-          borderRadius: "5px",
-          fontSize: "xx-large",
-          maxHeight: "fit-content"
-
-        }}
-        expandIcon="&#94;"
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-
-        <Typography ><Row ><Col className='ingredient-name'>{ingredient.name}</Col><Col className='ingredient-amount'>{ingredient.amount}</Col></Row> </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
+    <Accordion.Item eventKey={ingredient.key}>
+      <Accordion.Header>
+        <span id="ingredientName">{ingredient.name}</span>
+        <span id="ingredientAmount">{ingredient.amount}</span>
+      </Accordion.Header>
+      <Accordion.Body>
         {ingredient.subs && ingredient.subs.map(sub =>
-
-          <Row className='sub'>
-            <Col>
-              <Row>
-                <Col className='col-sm-auto'>{sub.name}</Col> <Col>{sub.effect}</Col>
-              </Row>
-            </Col>
-            <Col >
-              <button type="button" class="btn btn-sm info-button" data-toggle="modal" data-target="#exampleModal">
-                i
-              </button>
-            </Col>
-          </Row>
+          <Container id="sub">
+            <div>
+              <p id="subName">{sub.name}</p>
+              <p id="subEffect">{sub.effect}</p>
+            </div>
+            <span className="material-symbols-outlined" id="infoIcon">
+              info
+            </span>
+          </Container>
         )}
-
-      </AccordionDetails>
-
-    </Accordion>
+      </Accordion.Body>
+    </Accordion.Item>
   );
 }
 
-export default function Recipe() {
-
+function Recipe() {
   let [subs, setSubs] = useState(
     [
       {
@@ -141,16 +109,14 @@ export default function Recipe() {
   let [recipe, setRecipe] = useState({
     name: "Orange Chicken",
     price: "$$",
-    time: "30min",
+    time: "30 min",
     health: 2,
-    image: { chickenPic },
     favorite: false
   })
 
   let [favorite, setFavorite] = useState(false);
 
   let navigate = useNavigate();
-
 
   const SaveButtonClick = (event) => {
     setFavorite(!favorite);
@@ -162,69 +128,85 @@ export default function Recipe() {
 
   const StarSelector = () => {
     if (favorite) {
-      return savedYellowButton;
+      return (
+        <span className="material-symbols-outlined" id="saveIconFilled">
+          star
+        </span>
+      );
     } else {
-      return savedButton;
+      return (
+        <span className="material-symbols-outlined" id="saveIcon">
+          star
+        </span>
+      );
     }
   }
 
   return (
-    <div>
+    <Container>
       {/* BACK AND SAVE BUTTONS */}
-      <Container>
-        <Row>
-          <Col className=' back-button'>
-            <Link onClick={BackButtonClick}><img src={backButton} ></img></Link>
-          </Col>
-          <Col className=' save-button'>
-            <Link onClick={SaveButtonClick}><img src={StarSelector()} ></img></Link>
-          </Col>
-        </Row>
+      <Row>
+        <Col id="backCol">
+          <Button onClick={BackButtonClick} id="backBtn">
+            <span className="material-symbols-outlined" id="backIcon">
+              arrow_back
+            </span>
+          </Button>
+        </Col>
+        <Col id="saveCol">
+          <Button onClick={SaveButtonClick} id="saveBtn">{StarSelector()}</Button>
+        </Col>
+      </Row>
 
-        {/* SEARCH BAR */}
-        <Form>
-          <InputGroup>
-            <Form.Control type="text" />
-            <Button variant="outline-secondary" id="searchBtn" type="submit">
-              <span className="material-symbols-outlined ">
-                search
-              </span>
-            </Button>
-          </InputGroup>
-        </Form>
+      {/* SEARCH BAR */}
+      <Form>
+        <InputGroup>
+          <Form.Control type="text" />
+          <Button variant="outline-secondary" id="searchBtn" type="submit">
+            <span className="material-symbols-outlined">
+              search
+            </span>
+          </Button>
+        </InputGroup>
+      </Form>
 
-        {/* FOOD IMAGE  */}
-        <div className="card bg-dark text-white">
-          <img className='card-img food-image' src={chickenPic} alt="Card image" />
-          <div className='card-img-overlay'>
-            <h5 className='card-title recipe-name'>{recipe.name}</h5>
-            <Row>
-              <Col>
-                <p className='card-text col-sm-2 recipe-price'>Price <p className="dollars">{recipe.price}</p></p>
-              </Col>
-              <Col>
-                <p className='card-text col-sm-2 recipe-time'>Time<p className="minutes">{recipe.time}</p></p>
-              </Col>
-              <Col>
-                <p className='card-text col-sm-2 recipe-health'> Health <p className="leaves">{recipe.health}</p> </p>
-              </Col>
-            </Row>
+      <h1 id="titleText">{recipe.name}</h1>
+
+      {/* FOOD IMAGE  */}
+      <Image id="recipeImage" src="https://www.modernhoney.com/wp-content/uploads/2018/01/Chinese-Orange-Chicken-2.jpg" />
+
+      <div id="recipeInfo">
+        <div id="stat">
+          <div className="material-symbols-outlined" id="statIcon">
+            payments
           </div>
+          <p id="statText">{recipe.price}</p>
         </div>
-
-        <h1>Ingredients and Substitutes</h1>
-
-        {subs && subs.map(ingredient =>
-          <div className='ingredient-card'>
-            <Ingredient key={ingredient.id} ingredient={ingredient} />
+        <div id="stat">
+          <div className="material-symbols-outlined" id="statIcon">
+            eco
           </div>
-        )}
+          <p id="statText">{recipe.health}</p>
+        </div>
+        <div id="stat">
+          <div className="material-symbols-outlined" id="statIcon">
+            schedule
+          </div>
+          <p id="statText">{recipe.time}</p>
+        </div>
+      </div>
 
-      </Container>
-    </div>
+      <h3>Ingredients</h3>
+
+      <div id="scrollableContent">
+        {subs && subs.map(ingredient =>
+          <Accordion>
+            <Ingredient key={ingredient.id} ingredient={ingredient} />
+          </Accordion>
+        )}
+      </div>
+    </Container>
   );
 }
 
-
-
-
+export default Recipe;
