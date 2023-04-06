64e23f83ad4cb9350c20544a57c3b07d3fbb99e4
alejandro.cantu1018@gmail.com <Alejandro Cantu>
2023-04-06 11:19:19 -0500

    If user logged in, will pull favorites and star

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,6 +1,6 @@
 import './Recipe.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import {
   Accordion,
   Button,
@@ -11,6 +11,64 @@ import {
   Col,
   Image,
 } from 'react-bootstrap';
+import { initializeApp } from "firebase/app";
+import { getAuth , onAuthStateChanged } from "firebase/auth";
+import
+  {
+    getDatabase,
+    ref,
+    get,
+    child
+  } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlPgsYfKfSl15rkPbRzdX_7pjf3N5i424",
+  authDomain: "adaptipla.firebaseapp.com",
+  databaseURL: "https://adaptipla-default-rtdb.firebaseio.com",
+  projectId: "adaptipla",
+  storageBucket: "adaptipla.appspot.com",
+  messagingSenderId: "826210962785",
+  appId: "1:826210962785:web:1ba40a0510126f3dc54920",
+  measurementId: "G-235FB92HNR"
+};
+
+// Initialize Firebase
+const app = initializeApp( firebaseConfig );
+
+const auth = getAuth(app);
+
+const db = getDatabase();
+const dbRef = ref(db);
+
+
+onAuthStateChanged(auth, (user) => {
+
+  if (user) {
+    // User is signed in
+    const uid = user.uid;
+    console.log("UID: " + uid + " Name: " + user.displayName + " logged on!");
+  } else {
+    // User is not signed in
+    console.log("nope!");
+  }
+});
+
+// const DBQuery = (path) => {
+//   console.log(path);
+//   get(child(dbRef, path)).then((snapshot) => {
+//     if (snapshot.exists && snapshot.val()) {
+//       console.log(snapshot.val())
+//       return snapshot.val();
+//     } else {
+//       console.error("Error: DB data could not be accessed");
+//       return -1;
+//     }
+//   }).catch((error) => {
+//     console.error("DB Query Error: " + error);
+//     return -1;
+//   })
+// };
+
 
 const Ingredient = ({ ingredient }) => {
   return (
@@ -105,13 +163,55 @@ function Recipe() {
     name: "Orange Chicken",
     price: "$$",
     time: "30 min",
-    health: 2,
-    favorite: false
+    health: "moderate",
+    img: "https://www.modernhoney.com/wp-content/uploads/2018/01/Chinese-Orange-Chicken-2.jpg"
   })
+
+  let [dataLoaded, setDataLoaded] = useState(false);
 
   let [favorite, setFavorite] = useState(false);
 
+  let [loggedIn, setLoggedIn] = useState({});
+
   let navigate = useNavigate();
+
+  const LoadRecipeData = (() => {
+    get(child(dbRef, 'users/-NSD9UtFvXgxz4Mwgbmm')).then((userSnapshot) => {
+      console.log("joe")
+      if (userSnapshot.exists) {
+        if (userSnapshot.val().recipes) {
+          const recipes = [];
+          console.log(userSnapshot.val().recipes[0].id);
+          for (let favRecipe in userSnapshot.val().recipes) {
+            let key = userSnapshot.val().recipes[favRecipe].id;
+            get(child(dbRef, 'recipes/' + key)).then((recipeSnapshot) => {
+              console.log(recipeSnapshot.val());
+              if (recipeSnapshot && recipeSnapshot.val().name === recipe.name) {
+                setFavorite(true);
+              }
+            })
+          }
+        }
+      } else {
+        console.log("Failure to query DB");
+      }
+    }).catch((error) => {
+      console.error(error);
+    })
+  })
+
+  useEffect(() => {
+
+    console.log("=> useEffect!");
+    
+    if (auth.currentUser) {
+      setLoggedIn(auth.currentUser.uid);
+    }
+    LoadRecipeData();
+
+  }, [LoadRecipeData]);
+
+
 
   const SaveButtonClick = (event) => {
     setFavorite(!favorite);
@@ -168,7 +268,7 @@ function Recipe() {
       <h1 id="recipeTitleText">{recipe.name}</h1>
 
       {/* FOOD IMAGE  */}
-      <Image id="recipeImage" src="https://www.modernhoney.com/wp-content/uploads/2018/01/Chinese-Orange-Chicken-2.jpg" />
+      <Image id="recipeImage" src={recipe.img} />
 
       <div id="recipeInfo">
         <div id="stat">
