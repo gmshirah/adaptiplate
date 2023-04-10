0481726f8fee33302fc781ffa0fe912989a0c903
abeshilu <102710800+abeshilu@users.noreply.github.com>
2023-04-10 13:58:10 -0500

    keyword bs URL search checl

---
+++ src/pages/Search.js
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+function Search ()
+{
+    return <Container>
+        <h1>Place Holder
+        </h1>
+    </Container>
+}
+
+export default Search;
